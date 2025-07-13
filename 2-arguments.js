2a6bb9885f79e3910af9219529e4850ce9964f1c
Samuel Akongbota <122745519+Reysam2@users.noreply.github.com>
2025-07-13 09:30:17 +0000

    fix: rendering issue with codebase

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,15 +1,12 @@
-let arguments = function (a, b) {
-  switch (arguments.length) {
-    case 0:
-      console.log("No argument");
-      break;
-
-    case 1:
-      console.log("Argument found");
-      break;
-
-    default:
-      console.log("Arguments found");
+ let arguments = function (a, b) {
+  if (arguments.length === 0) {
+    console.log("No argument");
+  } else if (arguments.length === 1) {
+    console.log("Argument found");
+  } else {
+    console.log("Arguments found");
   }
-};
-console.log(arguments(1, 2));
+}; 
+arguments('a', 'l', 'x')
+arguments(1)
+arguments();
