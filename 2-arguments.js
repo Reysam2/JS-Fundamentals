2c90af36c2c32859026570270928372e5a697bac
Samuel Akongbota <122745519+Reysam2@users.noreply.github.com>
2025-07-13 09:47:47 +0000

    fix: add process.argv to codebase

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,12 +1,9 @@
- let arguments = function (a, b) {
-  if (arguments.length === 0) {
-    console.log("No argument");
-  } else if (arguments.length === 1) {
-    console.log("Argument found");
-  } else {
-    console.log("Arguments found");
-  }
-}; 
-arguments('a', 'l', 'x')
-arguments(1)
-arguments();
+const args = process.argv.slice(2); 
+
+if (args.length === 0) {
+  console.log("No argument");
+} else if (args.length === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
