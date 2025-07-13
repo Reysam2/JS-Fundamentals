3c2a2d5d0de40ab913cdb0bb9f75fa0987eb490b
Samuel Akongbota <122745519+Reysam2@users.noreply.github.com>
2025-07-13 07:15:41 +0000

    doc: update arguments page

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,8 +1,15 @@
-let i = 20
-if (i < 16) {
-  console.log("No argument");
-} else if (i > 16) {
-  console.log("Argument found");
-} else {
-  console.log("Arguments found");
-}
+let arguments = function (a, b) {
+  switch (arguments.length) {
+    case 0:
+      console.log("No argument");
+      break;
+
+    case 1:
+      console.log("Argument found");
+      break;
+
+    default:
+      console.log("Arguments found");
+  }
+};
+console.log(arguments(1, 2));
